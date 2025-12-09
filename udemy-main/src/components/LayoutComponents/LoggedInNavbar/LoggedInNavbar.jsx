61566603dd7360e5cd3baab91a11973bbb2ac2ad
Narasimha4228 <chilamkurinarasimhareddy@gmail.com>
2025-12-09 13:13:22 +0530

    Update user information in LoggedInNavbar

--- udemy-main/src/components/LayoutComponents/LoggedInNavbar/LoggedInNavbar.jsx
+++ udemy-main/src/components/LayoutComponents/LoggedInNavbar/LoggedInNavbar.jsx
@@ -71,8 +71,8 @@ const LoggedInNavbar = () => {
                     />
                   </div>
                   <div className={css.rightUserDiv}>
-                    <div className={css.uname}>Achi Venkata Eswar</div>
-                    <div className={css.email}>venkataeswarachi@gmail.com</div>
+                    <div className={css.uname}>Chilamkuri Narasimha Reddy</div>
+                    <div className={css.email}>chilamkurinarasimhareddy@gmail.com</div>
                   </div>
                 </Link>
               </div>
